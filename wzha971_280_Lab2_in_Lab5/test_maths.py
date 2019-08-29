c4dbfb235d9d45dd5ec7e6ec94f9a1139a279a01
wzha971 <wzha971@bcld03.uoa.auckland.ac.nz>
2019-08-29 16:44:12 +1200

    first try

---
+++ wzha971_280_Lab2_in_Lab5/test_maths.py
@@ -0,0 +1,28 @@
+import unittest     # Import the Python unit testing framework
+import maths        # Our code to test
+
+
+class MathsTest(unittest.TestCase):
+    ''' Unit tests for our maths functions. '''
+
+    def test_add(self):
+        ''' Tests the add function. '''
+        # Arrange
+        result = maths.add(2, 3)
+        result1 = maths.add(1,0)
+        result2 = maths.add(-1,1)
+        # Act
+        
+        # Assert
+        self.assertEqual(result, 5)
+        self.assertEqual(result1,1)
+        self.assertEqual(result2,0)
+
+    def test_fibonacci(self):
+        ''' Tests the fibonacci function. '''
+        pass # TODO
+
+
+# This allows running the unit tests from the command line (python test_maths.py)
+if __name__ == '__main__':
+    unittest.main()
